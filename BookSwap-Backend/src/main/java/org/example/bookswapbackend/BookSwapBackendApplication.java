9d1ce2b7bb00bc3060d40ba79a6148ed6a81c428
Barath <kvbarath2004@gmail.com>
2025-03-31 21:31:01 +0530

    initial commit

---
+++ BookSwap-Backend/src/main/java/org/example/bookswapbackend/BookSwapBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookswapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSwapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSwapBackendApplication.class, args);
+    }
+
+}
---
+++ BookSwap-Backend/src/test/java/org/example/bookswapbackend/BookSwapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookswapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSwapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
