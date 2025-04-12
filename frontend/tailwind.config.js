aa10265e99a63bd746947abbfa19d465a06b1233
LalithAbhishekG <lalithabhishek04@gmail.com>
2025-04-12 16:54:29 +0530

    Login page done

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'], // Set Outfit as the default sans-serif font
+    },
+  },
+  plugins: [],
+}
+
